12c42c57f52985d267cc7f2807d6c0b26afbf037
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-13 04:29:06 +0100

    Update lowfraghp.c

--- antidebug/memory/lowfraghp.c
+++ antidebug/memory/lowfraghp.c
@@ -1,5 +1,63 @@
 #include "lowfraghp.h"
 
+static BOOL IsWindowsVista() {
+	OSVERSIONINFOEX osvi;
+	DWORDLONG dwlConditionMask = 0;
+	int op = VER_EQUAL;
+
+	ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
+	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+	osvi.dwMajorVersion = 6;
+	osvi.dwMinorVersion = 0;
+
+	VER_SET_CONDITION(dwlConditionMask, VER_MAJORVERSION, op);
+	VER_SET_CONDITION(dwlConditionMask, VER_MINORVERSION, op);
+
+	return VerifyVersionInfo(
+		&osvi,
+		VER_MAJORVERSION | VER_MINORVERSION,
+		dwlConditionMask);
+}
+
+static BOOL IsWindows7() {
+	OSVERSIONINFOEX osvi;
+	DWORDLONG dwlConditionMask = 0;
+	int op = VER_EQUAL;
+
+	ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
+	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+	osvi.dwMajorVersion = 6;
+	osvi.dwMinorVersion = 1;
+
+	VER_SET_CONDITION(dwlConditionMask, VER_MAJORVERSION, op);
+	VER_SET_CONDITION(dwlConditionMask, VER_MINORVERSION, op);
+
+	return VerifyVersionInfo(
+		&osvi,
+		VER_MAJORVERSION | VER_MINORVERSION,
+		dwlConditionMask);
+}
+
+static BOOL IsWindows8or8PointOne() {
+	OSVERSIONINFOEX osvi;
+	DWORDLONG dwlConditionMask = 0;
+	int MajorOp = VER_EQUAL;
+	int MinorOp = VER_GREATER_EQUAL;
+
+	ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
+	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+	osvi.dwMajorVersion = 6;
+	osvi.dwMinorVersion = 2;
+
+	VER_SET_CONDITION(dwlConditionMask, VER_MAJORVERSION, MajorOp);
+	VER_SET_CONDITION(dwlConditionMask, VER_MINORVERSION, MinorOp);
+
+	return VerifyVersionInfo(
+		&osvi,
+		VER_MAJORVERSION | VER_MINORVERSION,
+		dwlConditionMask);
+}
+
 bool LowFragmentationHeap()
 {
 	PINT_PTR FrontEndHeap = NULL;
