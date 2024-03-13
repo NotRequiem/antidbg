79e54388a137a72bf4bd8b0579c420bfc23be9de
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-13 04:38:16 +0100

    Update unhexcp.h

--- antidebug/exceptions/unhexcp.h
+++ antidebug/exceptions/unhexcp.h
@@ -8,14 +8,6 @@ extern "C" {
 #include <stdio.h>
 #include <stdbool.h>
 
-	BOOL bIsBeinDbg = TRUE;
-
-	static LONG WINAPI UnhandledExcepFilter(PEXCEPTION_POINTERS pExcepPointers)
-	{
-		bIsBeinDbg = FALSE;
-		return EXCEPTION_CONTINUE_EXECUTION;
-	}
-
 	bool CheckUnhandledExcepFilter();
 
 #ifdef __cplusplus
