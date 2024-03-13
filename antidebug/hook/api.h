d7f838a58571c1873a94a40e26c68f0300198f09
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-13 04:12:27 +0100

    Add files via upload

---
+++ antidebug/hook/api.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <psapi.h>
+
+	bool CheckModuleBounds();
+
+#ifdef __cplusplus
+}
+#endif
