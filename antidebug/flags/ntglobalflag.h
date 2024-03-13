32044a5d16a1f1313bf088d4f668d67766ac8dfc
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-13 19:44:57 +0100

    Add files via upload

---
+++ antidebug/flags/ntglobalflag.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <Windows.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "..\adbg.h"
+
+	bool NtGlobalFlag();
+
+#ifdef __cplusplus
+}
+#endif
