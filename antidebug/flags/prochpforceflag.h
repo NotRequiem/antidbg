4b7eac4c9d9c4aeb63df5e41683451cdea88a1e1
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-13 01:47:51 +0100

    Add files via upload

---
+++ antidebug/flags/prochpforceflag.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <Windows.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+	bool ProcessHeapForceFlag();
+
+#ifdef __cplusplus
+}
+#endif
