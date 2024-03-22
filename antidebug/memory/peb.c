1048621a6ab93b4e4f796f4156d67014fa1ba76d
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-22 14:03:06 +0100

    added support for 32 bits

--- antidebug/memory/peb.c
+++ antidebug/memory/peb.c
@@ -1,6 +1,14 @@
 #include "peb.h"
 
+#if defined (BIT64)
+#define READ_PEB_OFFSET 0x60
+
+#elif defined(BIT32)
+#define READ_PEB_OFFSET 0x30
+
+#endif
+
 bool CheckPEB() {
-    PEB* peb = (PEB*)__readgsqword(0x60);
+    PEB* peb = (PEB*)__readgsqword(READ_PEB_OFFSET);
     return (*(BYTE*)((uintptr_t)peb + 2)) != 0;
 }
