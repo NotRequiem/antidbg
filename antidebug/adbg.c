9d0dd9a8bac7d694a0f8f06bc24a78cd1aa953e4
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-07-01 17:24:28 +0200

    Fixed false flags in antidebugger

--- antidebug/adbg.c
+++ antidebug/adbg.c
@@ -1,5 +1,3 @@
-/* Only reliable and tested methods, that can be done from user-mode, are included. */
-
 #include "adbg.h"
 
 #include "api\dbgpresent.h"
@@ -13,7 +11,6 @@
 #include "asm\sckreg.h"
 
 #include "exceptions\raiseexc.h"
-#include "exceptions\unhexcp.h"
 #include "exceptions\hwbreakp2.h"
 #include "exceptions\pgexcbp.h"
 
@@ -32,75 +29,103 @@
 #include "memory\membreakp.h"
 #include "memory\readstck.h"
 #include "memory\peb.h"
-#include "memory\lowfraghp.h"
 #include "memory\vrtalloc.h"
 
 #include "object\clshandle.h"
 #include "object\clsinvhandle.h"
 #include "object\crtfile.h"
 #include "object\dbgobj.h"
-#include "object\loadlib.h"
 #include "object\opnproc.h"
 #include "object\prothnd.h"
 #include "object\sysdbgctl.h"
 
 DebugCheckResult debuggerChecks[] = {
-    {"IsBeingDebugged", IsBeingDebugged, false},
-    {"IsRemoteDebuggerPresent", IsRemoteDebuggerPresent, false},
-    {"OutputDebugStringAPI", CheckOutputDebugString, false},
-    {"DebuggerBreak", DebuggerBreak, false},
-    {"int2D", int2D, false},
-    {"int3", int3, false},
-    {"POPFTrapFlag", POPFTrapFlag, false},
-    {"StackSegmentRegister", StackSegmentRegister, false},
-    // {"UnhandledExcepFilterTest", CheckUnhandledExcepFilter, false}, - Uncomment this if you're ok with having crashes when detecting debuggers
-    {"RaiseDbgControl", RaiseDbgControl, false},
-    {"PageExceptionBreakpoint", PageExceptionBreakpoint, false},
-    {"IsDebuggerPresent_DebugObjectHandle", IsDebuggerPresent_DebugObjectHandle, false},
-    {"KernelDebugger", KernelDebugger, false},
-    {"NtGlobalFlag", NtGlobalFlag, false},
-    {"IsDebuggerPresent_DebugFlags", IsDebuggerPresent_DebugFlags, false},
-    {"ProcessHeap_Flags", ProcessHeapFlag, false},
-    {"ProcessHeapForce_Flag", ProcessHeapForceFlag, false},
-    {"IsHooked", IsHooked, false},
-    {"API_Hooks", CheckModuleBounds, false},
-    {"ReadOwnMemoryStack", ReadMemoryStack, false},
-    {"PEB", CheckPEB, false},
-    // {"LowFragmentationHeap", LowFragmentationHeap, false}, - Not too reliable, uncomment if you wanna have more antidebugging protection with some false flags in edge cases
-    {"CheckNtQueryInformationProcess", CheckNtQueryInformationProcess, false},
-    {"HardwareBreakpoint", HardwareBreakpoint, false},
-    {"HardwareBreakpoint2", HardwareBreakPoint2, false},
-    {"MemoryBreakpoint", MemoryBreakpoint, false},
-    {"VirtualAlloc_MEM_WRITE_WATCH", WriteWatch, false},
-    {"CheckCloseHandle", CheckCloseHandle, false},
-    {"CheckCloseHandleWithInvalidHandle", CloseInvalidHandle, false},
-    {"CheckCreateFile", CheckCreateFile, false},
-    {"CheckNtQueryObject", CheckNtQueryObject, false},
-    {"CheckLoadLibrary", CheckLoadLibrary, false},
-    {"CheckOpenProcess", CheckOpenProcess, false},
-    {"SetHandleInformation", ProtectedHandle, false},
-    {"NtSystemDebugControl_Command", NtSystemDebugControl, false},
+    {false, "IsBeingDebugged", IsBeingDebugged},
+    {false, "IsRemoteDebuggerPresent", IsRemoteDebuggerPresent},
+    {false, "OutputDebugStringAPI", CheckOutputDebugString},
+    {false, "DebuggerBreak", DebuggerBreak},
+    {false, "int2D", int2D},
+    {false, "int3", int3},
+    {false, "POPFTrapFlag", POPFTrapFlag},
+    {false, "StackSegmentRegister", StackSegmentRegister},
+    {false, "RaiseDbgControl", RaiseDbgControl},
+    {false, "MemoryBreakpoint", MemoryBreakpoint},
+    {false, "PageExceptionBreakpoint", PageExceptionBreakpoint},
+    {false, "IsDebuggerPresent_DebugObjectHandle", IsDebuggerPresent_DebugObjectHandle},
+    {false, "KernelDebugger", KernelDebugger},
+    {false, "NtGlobalFlag", NtGlobalFlag},
+    {false, "IsDebuggerPresent_DebugFlags", IsDebuggerPresent_DebugFlags},
+    {false, "ProcessHeap_Flags", ProcessHeapFlag},
+    {false, "ProcessHeapForce_Flag", ProcessHeapForceFlag},
+    {false, "IsHooked", IsHooked},
+    {false, "API_Hooks", CheckModuleBounds},
+    {false, "ReadOwnMemoryStack", ReadMemoryStack},
+    {false, "PEB", CheckPEB},
+    {false, "CheckNtQueryInformationProcess", CheckNtQueryInformationProcess},
+    {false, "HardwareBreakpoint", HardwareBreakpoint},
+    {false, "HardwareBreakpoint2", HardwareBreakPoint2},
+    {false, "VirtualAlloc_MEM_WRITE_WATCH", WriteWatch},
+    {false, "CheckCloseHandle", CheckCloseHandle},
+    {false, "CheckCloseHandleWithInvalidHandle", CloseInvalidHandle},
+  //{false, "CheckCreateFile", CheckCreateFile}, -> Removed because if you open the program with SystemInformer, this technique flags it
+    {false, "CheckNtQueryObject", CheckNtQueryObject},
+    {false, "CheckOpenProcess", CheckOpenProcess},
+    {false, "SetHandleInformation", ProtectedHandle},
+    {false, "NtSystemDebugControl_Command", NtSystemDebugControl},
 };
 
-bool printDebugInfo = true;
+#define NUM_DEBUG_CHECKS (sizeof(debuggerChecks) / sizeof(debuggerChecks[0]))
 
-bool IsProgramDebugged() {
-    bool debuggerDetected = false;
+typedef NTSTATUS(NTAPI* pfnNtSetInformationThread)(
+    HANDLE ThreadHandle,
+    ULONG ThreadInformationClass,
+    PVOID ThreadInformation,
+    ULONG ThreadInformationLength
+    );
 
-    for (int i = 0; i < NUM_DEBUG_CHECKS; ++i) {
-        debuggerChecks[i].result = debuggerChecks[i].functionPtr();
-        if (debuggerChecks[i].result) {
-            debuggerDetected = true;
-            if (printDebugInfo) {
+pfnNtSetInformationThread pNtSetInformationThread = NULL;
+
+bool HideThreadFromDebugger() {
+    NTSTATUS status = pNtSetInformationThread(
+        NtCurrentThread,
+        ThreadHideFromDebugger,
+        NULL,
+        0
+    );
+
+    return NT_SUCCESS(status);
+}
+
+static DWORD WINAPI __spectrum_adbg(LPVOID lpParam) {
+    UNREFERENCED_PARAMETER(lpParam);
+
+    
+    HMODULE hNtdll = GetModuleHandleA("ntdll.dll");
+    if (hNtdll) pNtSetInformationThread = (pfnNtSetInformationThread)GetProcAddress(hNtdll, "NtSetInformationThread");
+    if (pNtSetInformationThread) HideThreadFromDebugger();
+    
+
+    while (1) {
+        for (int i = 0; i < NUM_DEBUG_CHECKS; ++i) {
+            debuggerChecks[i].result = debuggerChecks[i].functionPtr();
+            if (debuggerChecks[i].result) {
                 printf("%s: Debugger detected!\n", debuggerChecks[i].functionName);
+                __fastfail(1);
             }
-            break;
         }
+
+        Sleep(1000);
     }
 
-    if (!debuggerDetected && printDebugInfo) {
-        printf("No debugger detected.\n");
+    return 0;
+}
+
+void IsSpectrumDebugged() {
+    HANDLE hThread = CreateThread(NULL, 0, __spectrum_adbg, NULL, 0, NULL);
+    if (hThread == NULL) {
+        fprintf(stderr, "Error creating thread: %lu\n", GetLastError());
+        return;
     }
 
-    return debuggerDetected;
+    CloseHandle(hThread);   
 }
